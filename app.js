f0ad3ae90b31f03fc243eebf928828cc66d8f975
priyanshu Chandra <chandrapriyanshu10@gmail.com>
2023-08-07 13:07:39 +0530

    10th commit

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const path = require('path')
+const validator =  require('email-validator');
+
+
 
 const app = express()
 const port = process.env.PORT || 3000;
-
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static(__dirname + '/public'));
